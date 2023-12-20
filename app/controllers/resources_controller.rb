3879c0ed0b31d5050ec9c1d3f5f4079275448c8e
Joao Sagorro <joao.sagorro@gmail.com>
2023-12-20 15:23:57 +0000

    added actions to the resources controller

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,2 +1,35 @@
 class ResourcesController < ApplicationController
+  def index
+    # Allowed for every user (admin or not)
+    @resources = Resource.all
+  end
+
+  def show
+    # Allowed for every user (admin or not)
+    @resource = Resource.find(params[:id])
+  end
+
+  def new
+    # Allowed just for admin
+    @resource = Resource.new
+  end
+
+  def create
+    # Allowed just for admin
+    @resource =
+  end
+
+  def edit
+    # Allowed just for admin
+  end
+
+  def update
+    # Allowed just for admin
+  end
+
+  def destroy
+    # Allowed just for admin
+  end
+
+
 end
