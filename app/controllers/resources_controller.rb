ebb5283de32471686f597323161542926836f162
Joao Sagorro <joao.sagorro@gmail.com>
2023-12-21 08:52:23 +0000

    added index and new/create action with the right paths

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -16,20 +16,39 @@ class ResourcesController < ApplicationController
 
   def create
     # Allowed just for admin
-    @resource =
+    @user = User.find(params[:user_id])
+    @resource = Resource.new(set_params)
+    @resource.user = @user
+    @resource.save
+    if @resource.save
+      redirect_to resource_path(params[:id])
+    else
+      redirect_to :new, status: :unprocessable_entity
+    end
   end
 
   def edit
     # Allowed just for admin
+    @resource = Resource.find(params[:id])
   end
 
   def update
     # Allowed just for admin
+    # Revisit this, not sure if correct
+    @user = User.find(params[:user_id])
+    @resource = Resource.update(set_params)
+    @resource.user = @user
+    @resource.save
   end
 
   def destroy
     # Allowed just for admin
   end
 
+  private
+
+  def set_params
+    params.require(@resource).permit(:title, :category, :link)
+  end
 
 end
